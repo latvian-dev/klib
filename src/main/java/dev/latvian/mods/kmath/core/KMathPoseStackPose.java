4a306c47ad4971cd70513e2e6faa85c02980cbe6
LatvianModder <latvianmodder@gmail.com>
2025-05-22 16:59:41 +0300

    Optimization

--- src/main/java/dev/latvian/mods/kmath/core/KMathPoseStackPose.java
+++ src/main/java/dev/latvian/mods/kmath/core/KMathPoseStackPose.java
@@ -1,8 +1,15 @@
 package dev.latvian.mods.kmath.core;
 
 import dev.latvian.mods.kmath.vertex.VertexCallback;
+import org.joml.Matrix3f;
+import org.joml.Matrix3fc;
+import org.joml.Matrix4f;
+import org.joml.Matrix4fc;
 
 public interface KMathPoseStackPose {
+	Matrix3fc IDENTITY_3x3 = new Matrix3f();
+	Matrix4fc IDENTITY_4x4 = new Matrix4f();
+
 	default VertexCallback transform(VertexCallback callback) {
 		return transformNormals(transformPositions(callback));
 	}
--- src/main/java/dev/latvian/mods/kmath/core/mixin/PoseStackPoseMixin.java
+++ src/main/java/dev/latvian/mods/kmath/core/mixin/PoseStackPoseMixin.java
@@ -24,16 +24,39 @@ public class PoseStackPoseMixin implements KMathPoseStackPose {
 
 	@Override
 	public VertexCallback transform(VertexCallback callback) {
-		return callback.withTransformedPositionsAndNormals(pose, normal, trustedNormals);
+		boolean n = trustedNormals && normal.equals(KMathPoseStackPose.IDENTITY_3x3);
+		boolean p = pose.equals(KMathPoseStackPose.IDENTITY_4x4);
+
+		if (n && p) {
+			return callback;
+		} else if (n) {
+			return callback.withTransformedPositions(pose);
+		} else if (p) {
+			return callback.withTransformedNormals(normal, !trustedNormals);
+		} else {
+			return callback.withTransformedPositionsAndNormals(pose, normal, !trustedNormals);
+		}
 	}
 
 	@Override
 	public VertexCallback transformPositions(VertexCallback callback) {
-		return callback.withTransformedPositions(pose);
+		boolean p = pose.equals(KMathPoseStackPose.IDENTITY_4x4);
+
+		if (p) {
+			return callback;
+		} else {
+			return callback.withTransformedPositions(pose);
+		}
 	}
 
 	@Override
 	public VertexCallback transformNormals(VertexCallback callback) {
-		return callback.withTransformedNormals(normal, trustedNormals);
+		boolean n = trustedNormals && normal.equals(KMathPoseStackPose.IDENTITY_3x3);
+
+		if (n) {
+			return callback;
+		} else {
+			return callback.withTransformedNormals(normal, !trustedNormals);
+		}
 	}
 }
