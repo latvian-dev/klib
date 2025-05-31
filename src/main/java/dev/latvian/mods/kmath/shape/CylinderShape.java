2055ec2d6ba7ccebbe8da172fa76ab60cdcb2e06
LatvianModder <latvianmodder@gmail.com>
2025-05-31 20:19:40 +0300

    Increased cylinder detail

--- src/main/java/dev/latvian/mods/kmath/shape/CylinderShape.java
+++ src/main/java/dev/latvian/mods/kmath/shape/CylinderShape.java
@@ -68,9 +68,9 @@ public record CylinderShape(float radius, float height) implements Shape {
 	public void buildQuads(float x, float y, float z, VertexCallback callback) {
 		float r = Math.max(radius, 0F);
 		float h = Math.max(height, 0F) / 2F;
-		double rs = Math.PI * 2D / 24D;
+		double rs = Math.PI * 2D / 96D;
 
-		for (int i = 0; i < 24; i += 2) {
+		for (int i = 0; i < 96; i += 2) {
 			float cx = (float) (Math.cos(i * rs) * r);
 			float cz = (float) (Math.sin(i * rs) * r);
 			float nx = (float) (Math.cos((i + 1D) * rs) * r);
