0d8927dafd2c15bd4bb1d76353fd3170a695e39e
LatvianModder <latvianmodder@gmail.com>
2026-08-24 20:02:43 +0300

    Added more compress methods

--- src/main/java/dev/latvian/mods/klib/io/CompressionMethod.java
+++ src/main/java/dev/latvian/mods/klib/io/CompressionMethod.java
@@ -1,5 +1,6 @@
 package dev.latvian.mods.klib.io;
 
+import com.github.luben.zstd.Zstd;
 import com.github.luben.zstd.ZstdInputStream;
 import com.github.luben.zstd.ZstdOutputStream;
 import com.mojang.serialization.Codec;
@@ -14,9 +15,11 @@ import net.minecraft.util.FastBufferedInputStream;
 import net.minecraft.util.StringRepresentable;
 
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.GZIPInputStream;
@@ -24,11 +27,11 @@ import java.util.zip.GZIPOutputStream;
 import java.util.zip.InflaterInputStream;
 
 public enum CompressionMethod implements StringRepresentable {
-	NONE(0, "none", in -> in, out -> out),
-	GZIP(1, "gzip", GZIPInputStream::new, GZIPOutputStream::new),
-	DEFLATE(2, "deflate", InflaterInputStream::new, DeflaterOutputStream::new),
-	LZ4(3, "lz4", LZ4BlockInputStream::new, LZ4BlockOutputStream::new),
-	ZSTD(4, "zstd", ZstdInputStream::new, ZstdOutputStream::new),
+	NONE(0, "none", in -> in, out -> out, bytes -> bytes, buf -> buf),
+	GZIP(1, "gzip", GZIPInputStream::new, GZIPOutputStream::new, null, null),
+	DEFLATE(2, "deflate", InflaterInputStream::new, DeflaterOutputStream::new, null, null),
+	LZ4(3, "lz4", LZ4BlockInputStream::new, LZ4BlockOutputStream::new, null, null),
+	ZSTD(4, "zstd", ZstdInputStream::new, ZstdOutputStream::new, Zstd::compress, buf -> Zstd.compress(buf, Zstd.defaultCompressionLevel())),
 
 	;
 
@@ -52,12 +55,16 @@ public enum CompressionMethod implements StringRepresentable {
 	public final String name;
 	private final IOUnaryOperator<InputStream> input;
 	private final IOUnaryOperator<OutputStream> output;
+	private final IOUnaryOperator<byte[]> bytes;
+	private final IOUnaryOperator<ByteBuffer> byteBuffer;
 
-	CompressionMethod(int id, String name, IOUnaryOperator<InputStream> input, IOUnaryOperator<OutputStream> output) {
+	CompressionMethod(int id, String name, IOUnaryOperator<InputStream> input, IOUnaryOperator<OutputStream> output, IOUnaryOperator<byte[]> bytes, IOUnaryOperator<ByteBuffer> byteBuffer) {
 		this.id = id;
 		this.name = name;
 		this.input = input;
 		this.output = output;
+		this.bytes = bytes;
+		this.byteBuffer = byteBuffer;
 	}
 
 	@Override
@@ -79,5 +86,36 @@ public enum CompressionMethod implements StringRepresentable {
 
 	public OutputStream bufferedOut(OutputStream out) throws IOException {
 		return new BufferedOutputStream(out(out));
+	}
+
+	public byte[] compress(byte[] data) throws IOException {
+		if (bytes == null) {
+			var byteOut = new ByteArrayOutputStream(Math.max(64, data.length / 4));
+
+			try (var out = out(byteOut)) {
+				out.write(data);
+			}
+
+			return byteOut.toByteArray();
+		}
+
+		return bytes.apply(data);
+	}
+
+	public ByteBuffer compress(ByteBuffer data) throws IOException {
+		if (byteBuffer == null) {
+			if (data.hasArray() && data.position() == 0 && data.limit() == data.capacity()) {
+				int limit = data.limit();
+				var result = compress(data.array());
+				data.limit(limit);
+				return ByteBuffer.wrap(result);
+			} else {
+				var arr = new byte[data.remaining()];
+				data.get(arr);
+				return ByteBuffer.wrap(compress(arr));
+			}
+		}
+
+		return byteBuffer.apply(data);
 	}
 }
