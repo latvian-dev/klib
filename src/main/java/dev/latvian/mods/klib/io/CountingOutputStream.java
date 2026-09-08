fffabfe59b2d069f31f8911891b8a7776e817c6e
LatvianModder <latvianmodder@gmail.com>
2026-09-08 18:21:13 +0300

    Added CountingOutputStream

--- src/main/java/dev/latvian/mods/klib/io/CountingOutputStream.java
+++ src/main/java/dev/latvian/mods/klib/io/CountingOutputStream.java
@@ -2,24 +2,23 @@ package dev.latvian.mods.klib.io;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
 import java.io.OutputStream;
 
 public class CountingOutputStream extends OutputStream {
 	private long count = 0L;
 
 	@Override
-	public void write(int value) throws IOException {
+	public void write(int value) {
 		count++;
 	}
 
 	@Override
-	public void write(byte @NotNull [] b) throws IOException {
+	public void write(byte @NotNull [] b) {
 		count += b.length;
 	}
 
 	@Override
-	public void write(byte @NotNull [] b, int off, int len) throws IOException {
+	public void write(byte @NotNull [] b, int off, int len) {
 		count += len;
 	}
 
